ec8539044d192d699b08fbbcbcb60dcfa34d0759
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-08 17:26:54 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,22 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  
+  belongs_to :artist 
+  belongs_to :genre 
+  
+  def song_artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end 
+  
+  def song_artist_name
+    self.artist ? self.artist.name : nil 
+  end 
+  
+  def song_genre_id=(name)
+    self.genre = Genre.find(genre_id)
+  end 
+  
+  def song_genre_id
+    self.genre ? self.genre.name : nil 
+  end 
+  
 end
