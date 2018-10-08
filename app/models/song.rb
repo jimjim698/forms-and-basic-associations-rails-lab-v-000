926bf5a52c3e0824b3be7e3b1a95c7a800c6073b
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-08 18:18:39 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,20 +4,21 @@ class Song < ActiveRecord::Base
   belongs_to :genre 
   has_many :notes 
   
-  def song_artist_name=(name)
+  def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end 
   
-  def song_artist_name
+  def artist_name
     self.artist ? self.artist.name : nil 
   end 
   
-  ##  self.artist = Artist.find_or_create_by(name: name)
-  end 
+ # def artist_name=(name)
+  #  self.artist = Artist.find_or_create_by(name: name)
+  #end 
   
-  def artist_name
-    self.artist.name 
-  end 
+  #def artist_name
+   # self.artist.name 
+  #end 
   
   def song_genre_id=(name)
     self.genre = Genre.find(genre_id)
